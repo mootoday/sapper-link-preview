a835aca8d03c01774b0c350aa9ff3a1cf37ab634
Mike Nikles <mikenikles@gmail.com>
2020-04-17 15:15:44 +0100

    Add the LinkPreview component and examples.

---
+++ src/components/link-preview-card.svelte
@@ -0,0 +1,58 @@
+
+<script>
+	import { onMount } from "svelte";
+	
+	export let href;
+  export let show = false;
+	
+	let title;
+	let description;
+	let imgSrc;
+	
+	onMount(() => {
+    fetch("/link-preview.json?href=" + href)
+      .then(response => response.json())
+      .then(linkData => {
+        title = linkData.title;
+        description = linkData.description;
+        imgSrc = linkData.imgSrc;
+      });
+	});
+</script>
+
+<style>
+  .card {
+    width: 150px;
+    font-size: 10px;
+    color: black;
+
+    position: absolute;
+    z-index: 100;
+
+    bottom: 30px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .card img {
+    width: 150px;
+  }
+
+  .card-title {
+    font-size: 14px;
+  }
+</style>
+
+{#if show}
+	<div class="card">
+    {#if imgSrc}
+  		<img src={imgSrc} alt={title} class="card-img-top">
+    {/if}
+		<div class="card-body">
+			<h5 class="card-title">{title}</h5>
+      {#if description}
+  			<p class="card-text">{description}</p>
+      {/if}
+		</div>
+	</div>
+{/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,48 @@
+<script>
+  import LinkPreview from "../components/link-preview.svelte";
+
+  const linkPreviewExample = '<LinkPreview href="https://www.google.com">www.google.com</LinkPreview>';
+</script>
+
+<style>
+  .wrapper {
+    width: 500px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    position: absolute;
+  }
+</style>
+
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Sapper Link Preview</title>
 </svelte:head>
+
+<div class="wrapper">
+  <p>Hi there! 👋</p>
+  <p>
+    I was inspired by
+    <LinkPreview href="https://twitter.com/ilonacodes">@ilonacodes</LinkPreview>
+    and created this
+    <LinkPreview href="https://sapper.svelte.dev/">Sapper-version</LinkPreview>
+    of her frontend shorts.
+  </p>
+  <p>
+    With Sapper, and likely Vue, React, etc as well, we can leverage a backend
+    service to fetch URL preview information at runtime.
+    This allows us to define a link including its preview with a single component:
+  </p>
+  <pre>
+    {linkPreviewExample}
+  </pre>
+  <p>
+    Please check out
+    <LinkPreview href="https://dev.to/ilonacodes/frontend-shorts-vue-js-vanilla-js-digital-dices-og">
+      her post about frontend shorts
+    </LinkPreview>
+    series on
+    <LinkPreview href="https://dev.to">
+      dev.to.
+    </LinkPreview>
+  </p>
+</div>
